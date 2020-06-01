8fb3bdba38f9e7ef6d0d6d8bbf2124bf88e043b3
Orlando Falcone 20011705 <20011705@studenti.uniupo.it>
2020-06-01 14:09:37 +0200

    initial commit

---
+++ Controllers/JobExeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using slave1.Models;
+
+namespace slave1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobExeController : ControllerBase
+    {
+        // GET: api/JobExe
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/JobExe/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/JobExe
+        [HttpPost]
+        public async Task<IActionResult> Post(Job job)
+        {
+            
+            List<JobResult> jobResultList = new List<JobResult>();
+
+            foreach (var idNode in job.IdNodeList)
+            {
+                JobResult jobResult = new JobResult();
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = job.Path;
+                    process.StartInfo.Arguments = job.Argument;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.Start();
+
+                    jobResult.Pid = process.Id;
+                    jobResult.ExitCode = process.ExitCode;
+                    jobResult.StandardOutput = process.StandardOutput.ReadToEnd();
+                    jobResult.IdNode = idNode;
+                    jobResultList.Add(jobResult);
+
+                    process.WaitForExit();
+                }   
+            }
+            return Ok(jobResultList);
+        }
+
+        // PUT: api/JobExe/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace slave1.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Argument { get; set; }
+        public  List<int> IdNodeList { get; set; }
+    }
+}
