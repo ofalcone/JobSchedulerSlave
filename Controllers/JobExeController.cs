6586fd97f4dfb5db6e85794e82d3083041239e7c
Stefano Furlan <20010807@studenti.uniupo.it>
2020-06-15 19:57:31 +0200

    JobExeController

--- Controllers/JobExeController.cs
+++ Controllers/JobExeController.cs
@@ -10,7 +10,7 @@ using slave1.Models;
 
 namespace slave1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class JobExeController : ControllerBase
     {
@@ -30,14 +30,36 @@ namespace slave1.Controllers
 
         // POST: api/JobExe
         [HttpPost]
-        public async Task<IActionResult> Post(Job job)
+        public IActionResult StartJob(Job job)
         {
-            
+
             List<JobResult> jobResultList = new List<JobResult>();
 
             foreach (var idNode in job.IdNodeList)
             {
                 JobResult jobResult = new JobResult();
+
+                //Task.Run(() =>
+                //{
+                //    using (Process process = new Process())
+                //    {
+                //        process.StartInfo.FileName = job.Path;
+                //        process.StartInfo.Arguments = job.Argument;
+                //        process.StartInfo.UseShellExecute = false;
+                //        process.StartInfo.RedirectStandardOutput = true;
+                //        jobResult.Pid = process.Id;
+
+                //        process.Start();
+                //        //process.WaitForExit();
+
+
+                //        //jobResult.ExitCode = process.ExitCode;
+                //        jobResult.StandardOutput = process.StandardOutput.ReadToEnd();
+                //        jobResult.IdNode = idNode;
+                //        jobResultList.Add(jobResult);
+
+                //    }
+                //});
 
                 using (Process process = new Process())
                 {
@@ -49,26 +71,41 @@ namespace slave1.Controllers
                     process.WaitForExit();
 
                     jobResult.Pid = process.Id;
-                    jobResult.ExitCode = process.ExitCode;
-                    jobResult.StandardOutput = process.StandardOutput.ReadToEnd();
+                    //jobResult.ExitCode = process.ExitCode;
+                    //jobResult.StandardOutput = process.StandardOutput.ReadToEnd();
                     jobResult.IdNode = idNode;
                     jobResultList.Add(jobResult);
 
-                }   
+                }
+
             }
             return Ok(jobResultList);
         }
 
-        // PUT: api/JobExe/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        //[HttpPost, Route("/KillJob")]
+        //[HttpPost,Route("api/[controller]/[action]")]
+        //[HttpPost("{pid}")]
+        [HttpPost]
+        public IActionResult KillJob(JobKill jobKill)
         {
+            bool killProcessTree = false;
+
+            if (jobKill.Pid == 0)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var processFound = System.Diagnostics.Process.GetProcessById(jobKill.Pid);
+                processFound.Kill(killProcessTree);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return Ok();
         }
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
