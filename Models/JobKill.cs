43a2cf3301154c1e4208822c1453b7fc32497153
Stefano Furlan <20010807@studenti.uniupo.it>
2020-06-15 19:59:52 +0200

    Fix

---
+++ Models/JobKill.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace slave1.Models
+{
+    public class JobKill
+    {
+        public int Pid { get; set; }
+    }
+}
